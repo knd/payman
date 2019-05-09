181901df22cdcf62111282bf69d6ec15c7458186
Brice Aldrich <bricealdrich@protonmail.com>
2019-05-09 19:45:47 -0400

    init

--- main.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "github.com/DefinitelyNotAGoat/payman/cmd"
-
-//Used to show how to use added features to Create Batch Payments. The signed operations are not injeced but rather returned as an array.
-func main() {
-	cmd.Execute()
-}
